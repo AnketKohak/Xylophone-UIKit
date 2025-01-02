e71426381db5ad2ae179ebddbfe5b1643db41c6b
AnketKohak <anketkohak40@gmail.com>
2025-01-02 20:38:22 +0530

    Initial commit

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -5,17 +5,27 @@
 //  Created by Angela Yu on 28/06/2019.
 //  Copyright © 2019 The App Brewery. All rights reserved.
 //
-
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    var player: AVAudioPlayer!
 
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
-
+    @IBAction func keyPreesesd(_ sender: UIButton) {
+       
+        playSound(btntext: sender.currentTitle!)
+       
+    }
     
-
+    func playSound(btntext : String) {
+        let url = Bundle.main.url(forResource: btntext, withExtension: "wav")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
+                
+    }
 }
-
